d245404468b8bb51862ceeb063308069943ccf96
LudaTverday <Ludachka22@gmail.com>
2023-08-04 11:22:04 +0300

    extract to small methods

--- src/telran/io/TextCommentsSeparation.java
+++ src/telran/io/TextCommentsSeparation.java
@@ -7,25 +7,33 @@ public class TextCommentsSeparation {
 		if(args.length < 3) {
 			System.out.println(args[0]+ " "+ args[1] +" "+ args[2]);
 			System.out.println("Usage: must be three arguments(source, destinationText, destinationComments)");
-		} else {
-			String regexComm = "[ ]*(//).*";
-			try (BufferedReader reader = new BufferedReader(new FileReader(args[0]));
-					PrintWriter writerText = new PrintWriter(args[1]);
-					PrintWriter writerComments = new PrintWriter(args[2])){		
-				
-				reader.lines().filter(l -> !l.matches(regexComm)).forEach(l -> writerText.println(l));
-				reader.close();
-				BufferedReader reader1 = new BufferedReader(new FileReader(args[0]));
-				reader1.lines().filter(l -> l.matches(regexComm)).forEach(l -> writerComments.println(l) );
-				reader1.close();
+		} else {			
+			try (BufferedReader reader = getBufferedReader(args[0]);
+					PrintWriter writerText = getWriter(args[1]);
+					PrintWriter writerComments = getWriter(args[2])
+				){				
+				reader.lines()
+				.forEach(line -> getSourceWriter(writerText, writerComments, line).println(line));
 			} catch (FileNotFoundException e) {				
 				e.printStackTrace();
 			} catch (IOException e) {
 				
 				e.printStackTrace();
 			}
-		}
-		
+		}		
 	}
 
+	private static PrintWriter getSourceWriter(PrintWriter writerText,
+			PrintWriter writerComments, String line) {
+		return line.strip().startsWith("//") ? writerComments : writerText;
+	}
+	
+	private static BufferedReader getBufferedReader(String source) throws FileNotFoundException {
+		return new BufferedReader(new FileReader(source));
+	}
+	private static PrintWriter getWriter(String source) throws FileNotFoundException {
+		return new  PrintWriter(source);
+	}
+	
+//String regexComm = "\s*\t*(//).*";
 }
