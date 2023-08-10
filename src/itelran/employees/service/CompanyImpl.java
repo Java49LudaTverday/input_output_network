262d3b37765c82d8988b19a7d2c0ba20f00a8287
LudaTverday <Ludachka22@gmail.com>
2023-08-10 09:28:01 +0300

    methods for employeesSalary(add/remove)

--- src/itelran/employees/service/CompanyImpl.java
+++ src/itelran/employees/service/CompanyImpl.java
@@ -9,18 +9,47 @@ import itelran.employees.dto.SalaryDistribution;
 public class CompanyImpl implements Company {
 	// LinkedHashMap - in ordered view
 	LinkedHashMap<Long, Employee> employees = new LinkedHashMap<>();
+	TreeMap<Integer, Collection<Employee>> employeesSalary = new TreeMap<>();
 
 	@Override
 	public boolean addEmployee(Employee empl) {
 		// if does not includes -> adds employee and returns null
 		// otherwise return referents to object;
-		return employees.putIfAbsent(empl.id(), empl) == null;
+		boolean res = false;
+		Employee emplRes = employees.putIfAbsent(empl.id(), empl);
+		if (emplRes == null) {
+			res = true;
+			addEmployeeSalary(empl);
+		}
+		return res;
+	}
+
+	private void addEmployeeSalary(Employee empl) {
+
+		// if mapping no -> add;
+		// if there is one -> no mapping;
+		int salary = empl.salary();
+		employeesSalary.computeIfAbsent(salary, k -> new HashSet<>())
+		.add(empl);
+
 	}
 
 	@Override
 	public Employee removeEmployee(long id) {
+		Employee res = employees.remove(id);
+		if (res != null) {
+			removeEmployeeSalary(res);
+		}
+		return res;
+	}
 
-		return employees.remove(id);
+	private void removeEmployeeSalary(Employee empl) {
+		int salary = empl.salary();
+		Collection<Employee> employeesCol = employeesSalary.get(salary);
+		employeesCol.remove(empl);
+		if(employeesCol.isEmpty()) {
+			employeesSalary.remove(salary);
+		}
 	}
 
 	@Override
@@ -40,21 +69,51 @@ public class CompanyImpl implements Company {
 
 		return employees.values().stream()
 				.collect(Collectors.groupingBy(Employee::department, Collectors.averagingInt(Employee::salary)))
-				.entrySet().stream()
-				.map(entry -> new DepartmentSalary(entry.getKey(), entry.getValue())).toList();
+				.entrySet().stream().map(entry -> new DepartmentSalary(entry.getKey(), entry.getValue())).toList();
 	}
 
 	@Override
 	public List<SalaryDistribution> getSalaryDistribution(int interval) {
 
 		return employees.values().stream()
-				.collect(Collectors.groupingBy(e -> e.salary()/interval,
-						Collectors.counting()))
-				.entrySet().stream()
-				.map(e -> new SalaryDistribution(e.getKey() * interval,
-						e.getKey() * interval + interval - 1, e.getValue().intValue()))
-				.sorted((sd1, sd2) -> Integer.compare(sd1.minSalary(), sd2.minSalary()))
-						.toList();
-	}	
+				.collect(Collectors.groupingBy(e -> e.salary() / interval, Collectors.counting())).entrySet().stream()
+				.map(e -> new SalaryDistribution(e.getKey() * interval, e.getKey() * interval + interval - 1,
+						e.getValue().intValue()))
+				.sorted((sd1, sd2) -> Integer.compare(sd1.minSalary(), sd2.minSalary())).toList();
+	}
+
+	@Override
+	public List<Employee> getEmployeesByDepartment(String department) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Employee> getEmployeesBySalary(int salaryFrom, int salaryTo) {
+		
+		return employeesSalary.subMap(salaryFrom, true, salaryTo, true)
+				.values().stream()
+				//.flatMap(Collection::stream)
+				.flatMap(col -> col.stream().sorted(Comparator.comparing(Employee::id)))
+				.toList();
+	}
+
+	@Override
+	public List<Employee> getEmployeesByAge(int ageFrom, int ageTo) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Employee updateSalary(long id, int newSalary) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Employee updateDepartment(long id, String newDepartment) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
