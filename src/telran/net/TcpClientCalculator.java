8ad0060381b18335a38105cae91a14856a3a47e1
LudaTverday <Ludachka22@gmail.com>
2023-08-25 11:33:21 +0300

    extracted methods for operator, operand and request

--- src/telran/net/TcpClientCalculator.java
+++ src/telran/net/TcpClientCalculator.java
@@ -1,18 +1,23 @@
 package telran.net;
+
 import java.net.*;
 import java.util.ArrayList;
 import java.io.*;
 import telran.view.*;
 import java.util.*;
+
 public class TcpClientCalculator {
-	public static String HOST = "localhost";
-	public static int PORT = 6000;
-	public static HashSet<String> operators = new HashSet<>(List.of("+", "-", "*", "/"));
+	
+	 static String HOST = "localhost";
+	 static int PORT = 6000;
+	 static final String NAME_ITEM = "Send response ";
+	 static HashSet<String> operators = new HashSet<>(List.of("+", "-", "*", "/"));
 
 	public static void main(String[] args) throws Exception {
-		try(Socket socket = new Socket(HOST, PORT);
+		try (Socket socket = new Socket(HOST, PORT);
 				PrintStream writer = new PrintStream(socket.getOutputStream());
-				BufferedReader reader = new BufferedReader(new InputStreamReader(socket.getInputStream()))) {
+				BufferedReader reader = new BufferedReader(new InputStreamReader(socket.getInputStream()))) 
+		      {
 			InputOutput io = new ConsoleInputOutput();
 			Menu menu = new Menu("Client calculator menu", getItems(io, writer, reader));
 			menu.perform(io);
@@ -20,21 +25,35 @@ public class TcpClientCalculator {
 	}
 
 	private static ArrayList<Item> getItems(InputOutput io, PrintStream writer, BufferedReader reader) {
-		
-		return new ArrayList<>(List.of(Item.of("Send response ", 
-				io1 -> {
-					String requestOperator = io.readString("Enter operator: " + operators);
-					Double firstOperand = io.readDouble("Enter first operand: ", "Operand must be any double number");
-					Double secondOperand = io.readDouble("Enter second operand: ", "Operand must be any double number");
-					writer.println(String.format("%s#%f#%f", requestOperator, firstOperand, secondOperand));
-					try {
-					String request = reader.readLine();
-					io.writeLine("Result: " + request);
-					} catch (Exception e) {
-						System.out.println(e.toString());
-					}
-				} ),
-				Item.ofExit()));
+
+		return new ArrayList<>(List.of(Item.of(NAME_ITEM, io1 -> {
+			
+			String requestOperator = getOperator(io);
+			Double firstOperand = getOperand(io, "Enter first operand: ");
+			Double secondOperand = getOperand(io, "Enter second operand: ");
+			writer.println(String.format("%s#%f#%f", requestOperator, firstOperand, secondOperand));
+			displayRequest(io, reader);
+			
+		}), Item.ofExit()));
 	}
+
+	private static String getOperator(InputOutput io) {		
+		return io.readString("Enter operator: " + operators, "Wrong operator ", operators );
+	}
+	
+	private static double getOperand(InputOutput io, String prompt) {
+		return io.readDouble(prompt, "Operand must be any double number");
+	}
+	
+	private static void displayRequest(InputOutput io, BufferedReader reader) {
+		try {
+			String request = reader.readLine();
+			io.writeLine("Result: " + request);
+		} catch (Exception e) {
+			System.out.println(e.toString());
+		}
+	}
+
+	
 
 }
